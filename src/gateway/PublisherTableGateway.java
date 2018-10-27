fb89a268600999f0efac6f96a7596438ab7cdedb
Jaynan <jaynan.sanjose@gmail.com>
2018-10-27 14:38:23 -0500

    fixes eclipse bs

--- src/gateway/PublisherTableGateway.java
+++ src/gateway/PublisherTableGateway.java
@@ -1,5 +1,40 @@
 package gateway;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Book;
+import model.Publisher;
+
 public class PublisherTableGateway {
+	
+	private Connection conn;
+	
+	public PublisherTableGateway(Connection conn) {
+		this.conn = conn;
+	}
+	
+	public List<Publisher> getBooks() throws SQLException {
+		
+		List<Publisher> publishers = new ArrayList<Publisher>();
+		//Our parametized query.
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery("select * from publisher order by id");
+		while(rs.next()) {
+			Publisher publisher = new Publisher(this);
+			publisher.setId(rs.getInt("publisher_id"));
+			publisher.setTitle(rs.getString("title"));
+			
+			publishers.add(publisher);
+		}
+		
+		rs.close();
+		st.close();
+		return publishers;
+	}
 
 }
--- src/model/Publisher.java
+++ src/model/Publisher.java
@@ -1,5 +1,42 @@
 package model;
 
+import gateway.PublisherTableGateway;
+
 public class Publisher {
 
+	private int id;
+	private String title;
+	
+	private PublisherTableGateway gateway;
+	
+	public Publisher() {
+		id = 0;
+		title = null;
+	}
+	
+	public Publisher(int id, String title) {
+		this.id = id;
+		this.title = title;
+	}
+	
+	public Publisher(PublisherTableGateway gateway) {
+		this.gateway = gateway;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
 }
